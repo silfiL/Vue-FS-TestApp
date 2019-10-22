c55317859a86e835b3242ab677b10c3313c2bbb9
tribeMobile <silfilangie04@gmail.com>
2019-10-22 16:28:12 +0700

    First Commit (Video 1-3)

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="new-employee">
+        <h1>New Employee</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'new-employee',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAWh9hLBdXYmwE2dsADEbvsc280W9koWFk",
+    authDomain: "vuefs-prod-878c1.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-878c1.firebaseio.com",
+    projectId: "vuefs-prod-878c1",
+    storageBucket: "vuefs-prod-878c1.appspot.com",
+    messagingSenderId: "772859127245",
+    appId: "1:772859127245:web:8e3fbfb0ca457d760cd084",
+    measurementId: "G-5JTKWNG6S5"
+}
